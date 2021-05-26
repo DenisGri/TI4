0ec57c22524a355e25bd27ce73426eeb7d41008f
Денис Григорьев <Denisgri25@outlook.com>
2021-05-26 05:32:55 +0300

    initial commit

---
+++ TI4/LFSR.cs
@@ -0,0 +1,36 @@
+﻿namespace TI4
+{
+    public class Lfsr
+    {
+        readonly bool[] _bits;
+        public Lfsr(int bitCount, string seed)
+        {
+            _bits = new bool[bitCount];
+
+            for (var i = 0; i < bitCount; i++)
+                _bits[i] = seed[i] == '1' ? true : false;
+
+        }
+
+        private char Shift()
+        {
+            var bnew = _bits[3] ^ _bits[0] & true;
+            var result = _bits[^1] ? '1' : '0';
+            for (var i = _bits.Length - 1; i > 0; i--)
+            {
+                _bits[i] = _bits[i - 1];
+            }
+            _bits[0] = bnew;
+            return result;
+        }
+        public char[] GetKey(int length)
+        {
+            var key = new char[length * 8];
+            for (var i = 0; i < length * 8; i++)
+            {
+                key[i] = Shift();
+            }
+            return key;
+        }
+    }
+}
--- TI4/Program.cs
+++ TI4/Program.cs
@@ -1,12 +1,99 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace TI4
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        private static void WriteToFile(byte[] text)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+              File.WriteAllBytes("../../File.txt", text);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private static byte[] GetFile()
+        {
+            byte[] fileData = null;
+            try
+            {
+               fileData = File.ReadAllBytes("../../File.txt");
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return fileData;
+        }
+
+        private static byte[] Encryption(byte[] text, byte[] key)
+       {
+            var charArray = new byte[text.Length];
+            for(var i = 0; i < text.Length; i++)
+            {
+
+                charArray[i] = (byte)(text[i] ^ key[i]);
+            }
+            return charArray;
+       }
+
+        private static byte[] BitsToBytes(string bitString)
+        {
+            return Enumerable.Range(0, bitString.Length / 8).
+                Select(pos => Convert.ToByte(
+                    bitString.Substring(pos * 8, 8),
+                    2)
+                ).ToArray();
+        }
+
+        private static string GetSecretKey()
+        {
+            string bits;
+            do
+            {
+                Console.WriteLine("Input number:");
+                var secretKey = Convert.ToInt32(Console.ReadLine());
+                bits = Convert.ToString(secretKey, 2);
+            } while (bits.Length != 24);
+            return bits;
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var text = GetFile();
+
+                var bits = GetSecretKey();
+                var length = text.Length;
+
+                var lfsr = new Lfsr(24, bits);
+                var key = lfsr.GetKey(length);
+
+                var cipherText = Encryption(text, BitsToBytes(new string(key)));
+
+                WriteToFile(cipherText);
+
+                text = GetFile();
+
+                var normalText = Encryption(text, BitsToBytes(new string(key)));
+                WriteToFile(normalText);
+                Console.WriteLine(Encoding.Default.GetString(cipherText));
+                Console.WriteLine(Encoding.Default.GetString(normalText));
+            }
+            catch
+            {
+                // ignored
+            }
+
+            Console.ReadLine();
         }
     }
 }
